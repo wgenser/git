88e5517d7678ecc276ceb274a1110ccdee0e9fb9
David_GENSER <wgenser.wgenser@gmail.com>
2016-06-09 16:09:17 +0200

    last version

---
+++ app/src/main/java/com/example/david_genser/git/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.david_genser.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// just a comment 2
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
