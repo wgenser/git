c43215da74063655fcadd68d8ab687d8bebc3b4d
David_GENSER <wgenser.wgenser@gmail.com>
2016-06-10 11:16:30 +0200

    Version 10.9 mit button und variablen

--- app/src/main/java/com/example/david_genser/git/MainActivity.java
+++ app/src/main/java/com/example/david_genser/git/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.david_genser.git;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TextView-Variable erzeugen
+        final TextView Anzeige1 = (TextView) findViewById(R.id.textView);
+
+        // Button-Variablen erzeugen
+        Button Knopf1 = (Button) findViewById(R.id.button);
+        Button Knopf2 = (Button) findViewById(R.id.button2);
+
+
     }
 }
